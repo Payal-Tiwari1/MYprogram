efe5789e1eefd9e5becccab3e23e293f14a399ad
Payal Tiwari <payaltiwari10784@gmail.com>
2026-03-16 17:54:24 +0530

    Routing without MVC

---
+++ RoutingWithoutMVC/RoutingWithoutMVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingWithoutMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        
+        public int Details(int id)
+        {
+            return id;
+        }
+
+        public ActionResult<int> Contact()
+        {
+            return (100);
+        }
+    }
+}
