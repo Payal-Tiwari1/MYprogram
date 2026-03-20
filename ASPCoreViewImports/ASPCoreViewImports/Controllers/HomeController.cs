1d07a231280f50ac28d14f773120db71c58e6052
Payal Tiwari <payaltiwari10784@gmail.com>
2026-03-20 17:05:46 +0530

    ViewImport

---
+++ ASPCoreViewImports/ASPCoreViewImports/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using ASPCoreViewImports.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreViewImports.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(){ studentId = 1, studentName = "John", studentGender = "Male" },
+                new Student(){ studentId = 2, studentName = "Jane", studentGender = "Female" },
+                new Student(){ studentId = 3, studentName = "Bob", studentGender = "Male" },
+                new Student(){ studentId = 4, studentName = "Alice", studentGender = "Female" },
+                new Student(){ studentId = 5, studentName = "Tom", studentGender = "Male" },
+            };
+            
+            return View(students);
+        }
+        public IActionResult About()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(){ studentId = 1, studentName = "John", studentGender = "Male" },
+                new Student(){ studentId = 2, studentName = "Jane", studentGender = "Female" },
+                new Student(){ studentId = 3, studentName = "Bob", studentGender = "Male" },
+                new Student(){ studentId = 4, studentName = "Alice", studentGender = "Female" },
+                new Student(){ studentId = 5, studentName = "Tom", studentGender = "Male" },
+            };
+
+            return View(students);
+        }
+        public IActionResult Contact()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(){ studentId = 1, studentName = "John", studentGender = "Male" },
+                new Student(){ studentId = 2, studentName = "Jane", studentGender = "Female" },
+                new Student(){ studentId = 3, studentName = "Bob", studentGender = "Male" },
+                new Student(){ studentId = 4, studentName = "Alice", studentGender = "Female" },
+                new Student(){ studentId = 5, studentName = "Tom", studentGender = "Male" },
+            };
+            
+            return View(students);
+        }
+    }
+}
---
+++ ASPCoreViewImports/ASPCoreViewImports/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPCoreViewImports.Models
+{
+    public class Student
+    {
+        public int studentId { get; set; }
+        public string studentName { get; set; }
+        public string studentGender { get; set; }
+    }
+}
