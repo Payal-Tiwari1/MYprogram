ccac44c9b903ebda9fa1da3e65132bbd5bbca106
Payal Tiwari <payaltiwari10784@gmail.com>
2026-03-24 16:31:33 +0530

    TagHelpers example

---
+++ TagHelpersDemo/TagHelpersDemo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace TagHelpersDemo.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public string Designation { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,Female
+    }
+}
