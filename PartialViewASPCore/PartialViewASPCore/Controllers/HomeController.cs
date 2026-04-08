9934b5b9351c6681b56d59015db4c5d3dbb2a020
Payal Tiwari <payaltiwari10784@gmail.com>
2026-04-08 12:55:10 +0530

    partial view with model

--- PartialViewASPCore/PartialViewASPCore/Controllers/HomeController.cs
+++ PartialViewASPCore/PartialViewASPCore/Controllers/HomeController.cs
@@ -16,6 +16,18 @@ namespace PartialViewASPCore.Controllers
             return View();
         }
 
+        public IActionResult Product()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() { Id = 101, ProductName = "Dress", Discription = "This is a Dress", Price = 2500, Image = "~/Images/istockphoto-1440977634-612x612.jpg" },
+                new Product() { Id = 201, ProductName = "Pot", Discription = "This is a Pot", Price = 1500, Image = "~/Images/clay-pot-372.jpg" },
+                new Product() { Id = 301, ProductName = "Shoe", Discription = "This is a Toy", Price = 3500, Image = "~/Images/jpg.jpg" },
+            };
+            
+            return View(products);
+        }
+
         public IActionResult Privacy()
         {
             return View();
---
+++ PartialViewASPCore/PartialViewASPCore/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace PartialViewASPCore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Discription { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
