3473416e1c1df640f33fb33375022f5482fc7d85
Payal Tiwari <payaltiwari10784@gmail.com>
2026-04-08 18:06:59 +0530

    ASP Core Web API

---
+++ ASPCoreWebAPI/ASPCoreWebAPI/Controllers/FruitsAPIController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsAPIController : ControllerBase
+    {
+        public List<string> fruits = new List<string>() 
+        { "Apple", 
+          "Banana", 
+          "Cherry" 
+        };
+        
+        [HttpGet]
+        public List<string> GetFruits()
+        {
+            return fruits;
+        }
+
+        
+        
+        [HttpGet("{index}")]
+        public string GetFruitsByIndex(int index)
+        {
+            return fruits.ElementAt(index);
+        }
+    }
+
+    
+}
